Incorrect attacker gradient bug fixed. Created attacker-specific features.

gradient/gradient_new.py
@@ -7,15 +7,13 @@ import math, random
 import argparse
 
 trials = 5;
-agent_role = 'DEF';
-opponent_role = 'ATT';
 
 def evaluateWeights(opponents, directory, init=False):
     avgGradient = None;
     avgPayoff = 0;
 
-    print opponent_role
-    print agent_role
+    global opponent_role
+    global agent_role
 
     for t in range(trials):
         if init and t > 0:
@@ -76,6 +74,8 @@ def doAscentRandomRestarts(opponents, directory, eqPayoff=0):
     
     # Should be a column vector
     size = currentGradient.size;
+    print "GRADIENT LENGTH (should be 19):" , size;
+
     print "deciding random restart points..."
 
     weights0 = np.zeros((size, 1));
@@ -115,14 +115,14 @@ def doAscent(weights, opponents, directory):
     bestWeights = None;
     bestPayoff = -np.inf;
     while not converged:
-        (weights, newPayoff, newDirection, converged) = doAscentLineSearch(weights, opponents, directory, .5, .025);        
+        (weights, newPayoff, newDirection) = doAscentLineSearch(weights, opponents, directory, .5, .025);        
         if newPayoff > bestPayoff:
             bestWeights = weights;
             bestPayoff = newPayoff;
 
         gradientNorm = newDirection.transpose().dot(newDirection);
         print "gradientNorm ", gradientNorm
-        converged = converged or (gradientNorm < .00000001);
+        converged = (gradientNorm < .00000001);
         if ((gradientNorm > 1000)):
             print "WARNING: Gradient exploded! Terminating..."
             converged = True;
@@ -154,12 +154,12 @@ def doAscentLineSearch(startWeights, opponents, directory, decayRate, stopParame
         print "step payoff:", stepPayoff
 
         if stepPayoff > stopCriterion:
-            return (step, stepPayoff, stepGradient, False);
+            return (step, stepPayoff, stepGradient);
 
         stepsize = decayRate*stepsize;
         print "decreasing step size to: ", stepsize
 
-    return (startWeights, startPayoff, direction, True);
+    return (startWeights, startPayoff, direction);
         
 def main():
     parser = argparse.ArgumentParser(description='Gradient Ascent on Security Environment');
@@ -174,6 +174,7 @@ def main():
     global opponent_role
     global agent_role
     global trials
+    trial = 5;
 
     agent_role = args['agent'];
     if agent_role not in ('ATT', 'DEF'):

modules/agents.py
@@ -126,7 +126,7 @@ class Defender(Agent):
 
     def getActionTime(self, time):
         self.knowledge.updateTime(time)
-        self.knowledge.calculateAllResourceFeaturesMatrix(1)
+        self.knowledge.calculateFeaturesMatrix(1)
         actionTime = self.decideAction(self.knowledge, self.stParam, True)
         if actionTime is None:
             return actionTime
@@ -167,7 +167,7 @@ class Learner(Agent):
             return knowledge.time + period
         else:
             assert(newPayoff is not None);
-            (features, map) = knowledge.calculateAllResourceFeaturesMatrix(period);
+            (features, map) = knowledge.calculateFeaturesMatrix(period);
             numActions = features.shape[0];
             numFeatures = features.shape[1];
             if self.weights is None:
@@ -175,6 +175,7 @@ class Learner(Agent):
             if self.Z is None:
                 self.Z = self.initWeights(numFeatures);
                 self.Grad = self.initWeights(numFeatures);
+            
             rawvalue = np.dot(features, self.weights);
             probabilities = self.softmax(rawvalue);
             # A lot of untupling going on here...
@@ -233,91 +234,3 @@ class LearnerAttacker(Learner, Attacker):
         Attacker.__init__(self, **params);
         Learner.__init__(self, **params);
 
-
-class LearnerDefenderOld(Defender):
-    def __init__(self, **params):
-        super(LearnerDefender, self).__init__(**params)
-        
-        self.weights = params["weights"];
-
-        self.Grad = None;
-        self.Z = None;
-
-        self.step = 0;
-        self.cumulativePayoff = 0;
-
-    def setStrategy(self):
-#        print "setting strategy to learning";
-        if hasattr(self, "strategy"):
-            self.decideAction = getattr(self, "learnerAction")
-
-    def getAction(self, reward=None):
-	return self.decideAction(self.knowledge, self.stParam, False, reward)
-
-    def learnerAction(self, knowledge, params, askTime, newPayoff = None):
-        params = params.split("_");
-        period = float(params[0]);
-        beta = float(params[1]);
-
-        if(askTime):
-            if knowledge.time < knowledge.previousTime:
-                return None
-            return knowledge.time + period
-        else:
-            assert(newPayoff is not None);
-            (features, map) = knowledge.calculateAllResourceFeaturesMatrix(period);
-            numActions = features.shape[0];
-            numFeatures = features.shape[1];
-            if self.weights is None:
-                self.weights = self.initWeights(numFeatures)
-            if self.Z is None:
-                self.Z = self.initWeights(numFeatures);
-                self.Grad = self.initWeights(numFeatures);
-	    print numFeatures
-            rawvalue = np.dot(features, self.weights);
-            probabilities = self.softmax(rawvalue);
-            # A lot of untupling going on here...
-            # pick is simply the result of a random draw according
-            # to probabilites. 
-            pick = np.nonzero(np.random.multinomial(\
-                    1,probabilities.transpose().tolist()[0]))[0][0];
-
-            name = map[pick];
-            if name in knowledge.getActiveResources():
-                action = name;
-            else:
-#                if name is not None:
-#                    print "Selected ", name, "but:", knowledge.resources[name]
-                action = None;
-            
-            reward = newPayoff - self.cumulativePayoff;
-            self.cumulativePayoff = newPayoff;
-            # Update gradient estimate
-            
-            self.Grad = self.Grad + (1/float(self.step+1))* \
-                (reward*self.Z - self.Grad);
-            self.step += 1;
-            
-            averageFeatures = np.zeros((1,numFeatures));
-
-            for i in range(numActions):
-                averageFeatures += probabilities[i,0]*features[i,:];
-
-            score = features[pick,:] - averageFeatures;
-#            print str(features[pick, :])
-            self.Z = beta*self.Z + score.transpose();
-
-            return action
-
-    def initWeights(self, k):
-        return np.zeros((k,1));
-
-
-    def softmax(self, scores):
-        max = np.amax(scores);
-        unnormalized = np.exp(scores - max);
-        z = np.sum(unnormalized);
-        return unnormalized/z;
-
-    def getGradient(self):
-        return self.Grad;

modules/knowledge_state.py
@@ -33,6 +33,7 @@ class KnowledgeState(object):
         self.time = args["time"]
         self.owner = owner
         self.alpha = args["alpha"]
+        self.downtime = args["downtime"];
 
         if "miss" in args:
             self.miss = args["miss"];
@@ -171,9 +172,9 @@ class KnowledgeState(object):
                     maxCount = self.resources[item][
                         "probes since last reimage"]
             assert(maxServer is not None)
-            return maxServer
+            return (maxServer, maxCount);
         else:
-            return None    
+            return (None, None);
 
     def getLastProbed(self, klist):
         #activeList = self.getActiveResources()
@@ -233,10 +234,11 @@ class KnowledgeState(object):
     def periodsElapsed(self, eventTime, period):
         return (self.time - eventTime)/period;
 
+
     # This function computes all the features that might be used by
     # a learning algorithm. These are features that are ``machine
     # independent.''
-    def calculateInvariantLearningFeatures(self, period):
+    def calculateDefenderInvariantFeatures(self, period):
         totalProbed = 0;
         
         #(P)eriods (E)lapsed (S)ince Post-Reimage (P)robe
@@ -329,9 +331,17 @@ class KnowledgeState(object):
 
         return (lastProbed, penultimateProbed);
 
+    def calculateFeaturesMatrix(self, period):
+        if self.owner == "ATT":
+            return self.calculateAttackerFeaturesMatrix(period);
+
+        if self.owner == "DEF":
+            return self.calculateDefenderFeaturesMatrix(period);
+
+
     # Return matrix of all features as a numpy matrix. 
-    def calculateAllResourceFeaturesMatrix(self, period):
-        invariantFeatures = self.calculateInvariantLearningFeatures(period);
+    def calculateDefenderFeaturesMatrix(self, period):
+        invariantFeatures = self.calculateDefenderInvariantFeatures(period);
         
         invariantFeaturesAsList = \
             [ invariantFeatures[key] for key in sorted(invariantFeatures.keys())];
@@ -346,7 +356,7 @@ class KnowledgeState(object):
             indexToNameMap[index] = name; index += 1;
 
             resourceSpecificFeatures = \
-                self.calculateResourceFeatures(name, period, invariantFeatures, lastProbed)
+                self.calculateDefenderResourceFeatures(name, period, invariantFeatures, lastProbed)
             allResourceFeatures = list(invariantFeaturesAsList);
             allResourceFeatures.extend(resourceSpecificFeatures);
             if allFeatures is None:
@@ -367,8 +377,91 @@ class KnowledgeState(object):
         
         return (allFeatures, indexToNameMap);
 
+    def calculateAttackerFeaturesMatrix(self, period):
+        lastProbed = self.calculateLastTwoResources();
+        maxProbed = self.getMaxProbed(self.resources)[1];
 
-    def calculateResourceFeatures(self, name, period, invariant, lastProbed):        
+        allFeatures = None;
+
+        periodsSinceAction = self.periodsElapsed(self.lastActionTime, period);
+        reimages = 0;
+        for name, info in self.resources.iteritems():
+            reimages += info["Reimage Count"];
+
+        invariantFeatures = [1, periodsSinceAction, reimages];
+
+        index = 1;
+        indexToNameMap = {};
+        for name in sorted(self.resources.keys()):
+            indexToNameMap[index] = name; index += 1;
+
+            resourceSpecificFeatures = \
+                self.calculateAttackerResourceFeatures(name, period, lastProbed, maxProbed);
+            allResourceFeatures = list(invariantFeatures);
+            allResourceFeatures.extend(resourceSpecificFeatures);
+            if allFeatures is None:
+                allFeatures = np.array(allResourceFeatures);
+            else:
+                allFeatures = np.vstack([allFeatures, np.array(allResourceFeatures)]);
+
+
+        numFeatures = allFeatures.shape[1];        
+        noneFeatures = [1];
+        noneFeatures.extend([0] * (numFeatures - 1));
+
+        allFeatures = np.vstack([np.array(noneFeatures), allFeatures]);
+        indexToNameMap[0] = None;        
+
+        if debug: 
+            print "All features: ", allFeatures;
+        
+        return (allFeatures, indexToNameMap);
+
+
+    def calculateAttackerResourceFeatures(self, name, period, lastProbed, maxProbe):
+        features = [0];
+
+        hasBeenProbed = int(self.resources[name]["status"] == "PROBED");
+        features.append(hasBeenProbed); 
+
+        compromised = int(self.resources[name]["status"] == "COMPR");
+        features.append(compromised);
+
+        uncompromised = int(self.resources[name]["status"] != "COMPR");
+        features.append(uncompromised);
+
+        periodsElapsedSinceProbed = \
+            self.periodsElapsed(self.getLastProbe(name), period);
+        features.append(periodsElapsedSinceProbed); 
+        features.append(uncompromised*periodsElapsedSinceProbed);
+        
+        isLast = int(lastProbed[0] == name);
+        features.append(isLast); 
+        features.append(uncompromised*isLast);
+        
+        probeCount = self.getProbesSinceLastReimage(name);
+        features.append(probeCount);
+        features.append(uncompromised*probeCount);
+        features.append(uncompromised*probeCount/float(periodsElapsedSinceProbed));
+        
+        isMaxProbe = int(probeCount == maxProbe);
+        features.append(isMaxProbe); 
+        features.append(uncompromised*isMaxProbe);
+        if maxProbe > 0:
+            unprobedness = 1 - float(probeCount)/maxProbe;
+        else:
+            unprobedness = 1;
+        
+        features.append(unprobedness);
+        features.append(uncompromised*unprobedness);
+        
+        itsDown = int(self.time < (self.getLastReimage(name) + self.downtime));
+        features.append(itsDown);
+
+        return features;
+        
+
+    def calculateDefenderResourceFeatures(self, name, period, invariant, lastProbed):        
         # invariant is a dictionary containing the invariant features, 
         # and is used to speed up computations. lastProbed a tuple of the 
         # names (possibly None) of the two last machines to be probed and 

runSimulator.py
@@ -102,7 +102,7 @@ def parseArgs():
     return params
 
 
-def writeJson(payoffs, gradient, obs, args):
+def writeJson(payoffs, attGradient, defGradient, obs, args):
     payoff = {"players": []}
     for name, strategy in args['attackerList'].iteritems():
         payoff['players'].append({
@@ -111,7 +111,7 @@ def writeJson(payoffs, gradient, obs, args):
             "strategy": strategy,
             # "Total Probes":payoffs['totalProbes'],
             "payoff": payoffs["ATT"],
-	    "gradient": gradient.transpose().tolist()
+	    "gradient": attGradient.transpose().tolist()
             })
 
 #	features = {}
@@ -125,7 +125,7 @@ def writeJson(payoffs, gradient, obs, args):
                 #"Total Downtime":payoffs['totalDownTime'],
                 "payoff": payoffs["DEF"],
                 #"features": features,
-	    	"gradient": gradient.transpose().tolist()
+	    	"gradient": defGradient.transpose().tolist()
                 })
 
             with open(args['IOFolder'] + "/observation_" + str(obs)
@@ -137,9 +137,10 @@ def writeJson(payoffs, gradient, obs, args):
 def runSimulator(params):
     sim = Simulator.Simulator(params)
     payoff = sim.simulate()
-    (gradient) = sim.getDefenderGradient();
+    (gradientA) = sim.getAttackerGradient();
+    (gradientD) = sim.getDefenderGradient();
 #    print sim.stateManager.getUtilState();
-    return (payoff, gradient)
+    return (payoff, gradientA, gradientD)
 
 
 def main():
@@ -147,6 +148,7 @@ def main():
     rps = int(args["runs per sample"])
     for i in range(args["samples"]):
         defGradient = None;
+        attGradient = None;
         cPayoff = {
             "totalProbes": 0,
             "totalDowntime": 0,
@@ -154,26 +156,38 @@ def main():
             "ATT": 0,
             }
         for j in range(0, rps):
-            (payoff, gradient) = runSimulator(args)
+            (payoff, gradientA, gradientD) = runSimulator(args)
             if defGradient is None:
-                defGradient = gradient;
+                defGradient = gradientD;
             else:
-                defGradient += gradient;
+                defGradient += gradientD;
             
+            if attGradient is None:
+                attGradient = gradientA;
+            else:
+                attGradient += gradientA;
+
             for k, v in payoff.iteritems():
                 cPayoff[k] += v
 	
 	if defGradient is not None:
         	defGradient /= rps
 	else:
-		defGradient = np.zeros(1) 
+		defGradient = np.zeros(1)
+
+	if attGradient is not None:
+        	attGradient /= rps
+	else:
+		attGradient = np.zeros(1)
+
+ 
 
         for k, v in cPayoff.iteritems():
             # print cPayoff[k]
             cPayoff[k] /= rps
             # print cPayoff[k]
         # print "\n\n"
-        writeJson(cPayoff, defGradient, i, args)
+        writeJson(cPayoff, attGradient, defGradient, i, args)
 
 if __name__ == '__main__':
     main()